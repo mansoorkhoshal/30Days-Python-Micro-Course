fe577f662ff99b60db7141609ded30664c20ef0c
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2026-08-30 21:17:46 +0500

    this is my first day of python learning

--- 01-python_programming.py
+++ 01-python_programming.py
@@ -3,3 +3,4 @@ print("Hello World");
 print("Welcome to my first python programming")
 
 print("Welcome dear this is my first day of python learning")
+print("Welcome dear this is my first day of python learning")
