31229c1482c99ce3e7bd8e4a676401ccf965fb0c
Mansoor Ahmad <mansoorahmad.dev44@gmail.com>
2026-09-03 20:48:29 +0500

    Add Python programming practice

---
+++ 02-python_programming.py
@@ -0,0 +1,23 @@
+"""
+x = "Fantasitc"
+
+def myFunc():
+    
+    global x
+    x = "Awesome"
+
+myFunc()
+
+print("Python is " + x)
+
+
+
+x = 5
+
+def myfunc():
+    print("Python got" , x , "Ranking in the all tech languages.")
+    
+myfunc()
+
+"""
+
